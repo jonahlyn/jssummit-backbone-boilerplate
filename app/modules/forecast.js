3ede183b4045e53bdb23b73516ca4fee47f74d73
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2012-11-15 17:10:27 -0700

    Incorporating the weather app into backbone boiler.

---
+++ app/modules/forecast.js
@@ -0,0 +1,114 @@
+// Forecast module
+define([
+  // Application.
+  "app"
+],
+
+// Map dependencies from above array.
+function(app) {
+
+  // Create a new module.
+  var Forecast = app.module();
+
+  // Default Model.
+  Forecast.Model = Backbone.Model.extend({
+    url: function() {
+        return "http://api.wunderground.com/api/7eaec3b21b154448/conditions/q/" + this.get("zip") + ".json";
+    },
+    parse : function( data, xhr ) {
+        var observation = data.current_observation;
+        return {
+            id: observation.display_location.zip,
+            url: observation.icon_url,
+            state: observation.display_location.state_name,
+            zip: observation.display_location.zip,
+            city: observation.display_location.city,
+            temperature: observation.temp_f    
+        };
+    },
+    sync: function(method, model, options){  
+        options.dataType = "jsonp";  
+        return Backbone.sync(method, model, options);  
+    },
+    validate: function (options) {
+        if (!options.zip) {
+           return "Please enter a zip code";
+        }
+    }
+  });
+
+  // Default Collection.
+  Forecast.Collection = Backbone.Collection.extend({
+    model: Forecast.Model
+  });
+
+  // Default View.
+  //Forecast.Views.Layout = Backbone.Layout.extend({
+    //template: "forecast"
+  //});
+  
+  // A single forecast item
+  Forecast.Views.ForecastItem = Backbone.View.extend({
+    //template: "forecast-item",
+    tagName: "tr",
+    template: _.template($("#forcast-template").html()),
+    initialize: function(){
+      //_.bindAll(this, "render");
+      this.model.fetch({success: this.render}); //
+    },
+    render: function(){
+      var content = this.template(this.model.toJSON());
+      console.log('content', content);
+      this.$el.html(content);
+      return this;
+    }
+  });
+  
+  // Forecasts Table
+  Forecast.Views.ForecastsView = Backbone.View.extend({
+    template: "forecast",
+    initialize: function(){
+      this.collection.on("add", this.addForecast, this);
+      //this.collection.on("remove", this.remove, this);
+    },
+    //render: function(model) {},
+    addForecast: function(model){
+      var view = new Forecast.Views.ForecastItem({id: model.get("zip"), model: model});
+      this.$("tbody").append(view.$el).fadeIn("slow");
+      return this;
+    }
+  });
+  
+  // Search Form
+  Forecast.Views.Search = Backbone.View.extend({
+    template: "search", // load the search.html template
+    events: {
+      "click #search": "addZip"
+    },
+    initialize: function(){
+      this.collection.on("add", this.clear, this);
+    },
+    addZip: function(e){
+      e.preventDefault();
+      
+      var zip = this.$("#zip").val();
+      this.model = new Forecast.Model();
+      this.model.on("error", this.toggleError, this);
+      
+      if( this.model.set({zip: zip}) ){
+        this.collection.add( this.model );
+        this.toggleError();
+      }
+    },
+    clear: function(){
+      this.$("#zip").val("");
+    },
+    toggleError: function(model, error){
+      this.$(".alert").text(error).toggle(!!error); // !!error is true if an error was sent
+    }
+  });
+
+  // Return the module for AMD compliance.
+  return Forecast;
+
+});
--- app/router.js
+++ app/router.js
@@ -4,10 +4,11 @@ define([
   
   // Modules
   "modules/test",
-  "modules/simple"
+  "modules/simple",
+  "modules/forecast"
 ],
 
-function(app, Test, Simple) {
+function(app, Test, Simple, Forecast) {
 
   // Defining the application router, you can attach sub routers here.
   var Router = Backbone.Router.extend({
@@ -16,9 +17,12 @@ function(app, Test, Simple) {
     },
 
   index: function() {
+    var forecasts = new Forecast.Collection();
     app.useLayout().setViews({
-      "header" : new Test.Views.Test(),
-      "main" : new Simple.Views.Layout()
+      //"header" : new Test.Views.Test(),
+      //"main" : new Simple.Views.Layout(),
+      "header": new Forecast.Views.Search({collection: forecasts}),
+      "main" : new Forecast.Views.ForecastsView({collection: forecasts})
     }).render();
   }
 
